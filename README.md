d61234964701a924345b037df1f58a6f093500fb
Ly Lam <llam@fairventures.ca>
2018-07-25 15:28:09 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # firebaseRTDBConfigurationStore
-Simple application settings or document storage using your Firebase project's Realtime database
+Simple application settings or document storage using your Firebase project's Realtime Database.  Global configurations will be stored under `/internal/global/` and user-specific configurations will be stored under `/internal/users/<userId>`.
+
+# Prerequisite
+You need to initialize access to Firebase services before using this module.  Something like this if using this from Firebase Cloud Functions:
+
+```ts
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+admin.initializeApp(functions.config().firebase);
+```
+
+# Usage
+1. Create and initialize the object.
+ 
+ ```ts
+ const currentUserId = '1234';
+ const settings = new FirebaseRTDBConfigurationStore(currentUserId);
+ return settings.init().then(() => {...});
+ ```
+
+1. Retrieve values by key or get it's default value if a value doesn't exist for the key.
+
+```ts
+return settings.getGlobalData('someKey', 'default value')
+.then(globalValue => ...);
+```
+```ts
+return settings.getUserData('someOtherKey', 'default value')
+.then(userValue => ...);
+```
+
+1. Set the key-value pair.
+
+```ts
+return settings.setGlobalData('someKey', 'some value')
+.then(globalValue => ...);
+```
+```ts
+return settings.setUserData('someOtherKey', 'some value')
+.then(userValue => ...);
+```
---
+++ index.ts
@@ -0,0 +1,58 @@
+import * as admin from 'firebase-admin';
+
+export interface IConfiguration {
+	init(): Promise<IConfiguration>;
+	setGlobalData<T>(key: string, value: T): Promise<T>;
+	getGlobalData<T>(key: string, defaultValue?: T): Promise<T>;
+	setUserData<T>(key: string, value: T): Promise<T>;
+	getUserData<T>(key: string, defaultValue?: T): Promise<T>;
+}
+
+export class FirebaseRTDBConfigurationStore {
+	db = null;
+
+	constructor(public userID: string) {}
+
+	private setData<T>(settingsPath: string, value: T): Promise<T> {
+		const dbPath = this.db.ref(settingsPath);
+		return dbPath.set(value).then(() => value);
+	}
+
+	private getData<T>(settingsPath: string, defaultValue?: T): Promise<T> {
+		const dbPath = this.db.ref(settingsPath);
+		return dbPath.once('value').then(data => {
+			const val = data.val();
+			if (val === null && defaultValue !== undefined) return this.setData(settingsPath, defaultValue);
+			return val;
+		});
+	}
+
+	init(): Promise<IConfiguration> {
+		this.db = admin.database();
+		return Promise.resolve(this);
+	}
+
+	private getGlobalSettingsPath(key: string): string {
+		return `internal/global/${key}`;
+	}
+
+	private getUserSettingsPath(key: string): string {
+		return `internal/user/${this.userID}/${key}`;
+	}
+
+	setGlobalData<T>(key: string, value: T): Promise<T> {
+		return this.setData<T>(this.getGlobalSettingsPath(key), value);
+	}
+
+	getGlobalData<T>(key: string, defaultValue?: T): Promise<T> {
+		return this.getData<T>(this.getGlobalSettingsPath(key), defaultValue);
+	}
+
+	setUserData<T>(key: string, value: T): Promise<T> {
+		return this.setData<T>(this.getUserSettingsPath(key), value);
+	}
+
+	getUserData<T>(key: string, defaultValue?: T): Promise<T> {
+		return this.getData<T>(this.getUserSettingsPath(key), defaultValue);
+	}
+}
