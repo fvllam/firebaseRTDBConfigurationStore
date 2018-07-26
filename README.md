b1b6795726ae801a0bce45645d4d5bf3396d5d87
Ly Lam <llam@fairventureslab.ca>
2018-07-25 23:15:39 -0400

    Explicitly initialize the firebase app.

--- README.md
+++ README.md
@@ -1,27 +1,20 @@
 # firebaseRTDBConfigurationStore
-Simple application settings or document storage using your Firebase project's Realtime Database.  Global configurations will be stored under `/internal/global/` and user-specific configurations will be stored under `/internal/users/<userId>`.
+
+Simple application settings or document storage using your Firebase project's Realtime Database. Global configurations will be stored under `/internal/global/` and user-specific configurations will be stored under `/internal/users/<userId>`.
 
 # Prerequisite
-You need to initialize access to Firebase services before using this module.  Something like this if using this from Firebase Cloud Functions:
 
-```ts
-import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
-function initializeFirebase() {
-	if (admin.apps.length === 0) {
-		admin.initializeApp(functions.config().firebase);
-	}
-}
-```
+This module should only be used within a Firebase project since it implicitly initializes the Firebase app to access the project's Firebase services.
 
 # Usage
+
 ## Create and initialize the object.
- 
- ```ts
- const currentUserId = '1234';
- const settings = new FirebaseRTDBConfigurationStore(initializeFirebase, currentUserId);
- return settings.init().then(() => {...});
- ```
+
+```ts
+const currentUserId = '1234';
+const settings = new FirebaseRTDBConfigurationStore(currentUserId);
+return settings.init().then(() => {...});
+```
 
 ## Retrieve values by key or get it's default value if a value doesn't exist for the key.
 
@@ -29,6 +22,7 @@ function initializeFirebase() {
 return settings.getGlobalData('someKey', 'default value')
 .then(globalValue => ...);
 ```
+
 ```ts
 return settings.getUserData('someOtherKey', 'default value')
 .then(userValue => ...);
@@ -40,6 +34,7 @@ return settings.getUserData('someOtherKey', 'default value')
 return settings.setGlobalData('someKey', 'some value')
 .then(globalValue => ...);
 ```
+
 ```ts
 return settings.setUserData('someOtherKey', 'some value')
 .then(userValue => ...);
--- index.ts
+++ index.ts
@@ -1,60 +1,67 @@
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin"
 
 export interface IConfiguration {
-	init(): Promise<IConfiguration>;
-	setGlobalData<T>(key: string, value: T): Promise<T>;
-	getGlobalData<T>(key: string, defaultValue?: T): Promise<T>;
-	setUserData<T>(key: string, value: T): Promise<T>;
-	getUserData<T>(key: string, defaultValue?: T): Promise<T>;
+  init(): Promise<IConfiguration>
+  setGlobalData<T>(key: string, value: T): Promise<T>
+  getGlobalData<T>(key: string, defaultValue?: T): Promise<T>
+  setUserData<T>(key: string, value: T): Promise<T>
+  getUserData<T>(key: string, defaultValue?: T): Promise<T>
+}
+
+export function initializeFirebase() {
+  if (admin.apps.length === 0) {
+    //admin.initializeApp(functions.config().firebase)
+    admin.initializeApp()
+    console.log("firebase app initialized", admin.apps.length)
+  } else console.log("firebase app ALREADY initialized", admin.apps.length)
 }
 
 export class FirebaseRTDBConfigurationStore {
-	db: admin.database.Database;
+  db: admin.database.Database
 
-	constructor(firebaseInitializer: () => void, public userID: string) {
-		firebaseInitializer();
-	}
+  constructor(public userID: string) {}
 
-	private setData<T>(settingsPath: string, value: T): Promise<T> {
-		const dbPath = this.db.ref(settingsPath);
-		return dbPath.set(value).then(() => value);
-	}
+  private setData<T>(settingsPath: string, value: T): Promise<T> {
+    const dbPath = this.db.ref(settingsPath)
+    return dbPath.set(value).then(() => value)
+  }
 
-	private getData<T>(settingsPath: string, defaultValue?: T): Promise<T> {
-		const dbPath = this.db.ref(settingsPath);
-		return dbPath.once('value').then(data => {
-			const val = data.val();
-			if (val === null && defaultValue !== undefined) return this.setData(settingsPath, defaultValue);
-			return val;
-		});
-	}
+  private getData<T>(settingsPath: string, defaultValue?: T): Promise<T> {
+    const dbPath = this.db.ref(settingsPath)
+    return dbPath.once("value").then(data => {
+      const val = data.val()
+      if (val === null && defaultValue !== undefined) return this.setData(settingsPath, defaultValue)
+      return val
+    })
+  }
 
-	init(): Promise<IConfiguration> {
-		this.db = admin.database();
-		return Promise.resolve(this);
-	}
+  init(): Promise<IConfiguration> {
+    initializeFirebase()
+    this.db = admin.database()
+    return Promise.resolve(this)
+  }
 
-	private getGlobalSettingsPath(key: string): string {
-		return `internal/global/${key}`;
-	}
+  private getGlobalSettingsPath(key: string): string {
+    return `internal/global/${key}`
+  }
 
-	private getUserSettingsPath(key: string): string {
-		return `internal/user/${this.userID}/${key}`;
-	}
+  private getUserSettingsPath(key: string): string {
+    return `internal/user/${this.userID}/${key}`
+  }
 
-	setGlobalData<T>(key: string, value: T): Promise<T> {
-		return this.setData<T>(this.getGlobalSettingsPath(key), value);
-	}
+  setGlobalData<T>(key: string, value: T): Promise<T> {
+    return this.setData<T>(this.getGlobalSettingsPath(key), value)
+  }
 
-	getGlobalData<T>(key: string, defaultValue?: T): Promise<T> {
-		return this.getData<T>(this.getGlobalSettingsPath(key), defaultValue);
-	}
+  getGlobalData<T>(key: string, defaultValue?: T): Promise<T> {
+    return this.getData<T>(this.getGlobalSettingsPath(key), defaultValue)
+  }
 
-	setUserData<T>(key: string, value: T): Promise<T> {
-		return this.setData<T>(this.getUserSettingsPath(key), value);
-	}
+  setUserData<T>(key: string, value: T): Promise<T> {
+    return this.setData<T>(this.getUserSettingsPath(key), value)
+  }
 
-	getUserData<T>(key: string, defaultValue?: T): Promise<T> {
-		return this.getData<T>(this.getUserSettingsPath(key), defaultValue);
-	}
+  getUserData<T>(key: string, defaultValue?: T): Promise<T> {
+    return this.getData<T>(this.getUserSettingsPath(key), defaultValue)
+  }
 }
