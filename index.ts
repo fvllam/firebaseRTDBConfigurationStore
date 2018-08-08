f409e59954ae1f9488b8a480bae04e504a9b836e
Ly Lam <llam@fairventures.ca>
2018-08-08 18:10:33 -0400

    move interfaces to external package

--- index.ts
+++ index.ts
@@ -1,67 +1,42 @@
-import * as admin from "firebase-admin"
+import * as admin from "firebase-admin";
+import {
+  BaseConfigurationStore,
+  IConfigurationStore
+} from "@fvlab/configurationstore";
 
-export interface IConfiguration {
-  init(): Promise<IConfiguration>
-  setGlobalData<T>(key: string, value: T): Promise<T>
-  getGlobalData<T>(key: string, defaultValue?: T): Promise<T>
-  setUserData<T>(key: string, value: T): Promise<T>
-  getUserData<T>(key: string, defaultValue?: T): Promise<T>
-}
-
-export function initializeFirebase() {
+export function initializeFirebase(): void {
   if (admin.apps.length === 0) {
-    //admin.initializeApp(functions.config().firebase)
-    admin.initializeApp()
-    console.log("firebase app initialized", admin.apps.length)
-  } else console.log("firebase app ALREADY initialized", admin.apps.length)
+    // admin.initializeApp(functions.config().firebase)
+    admin.initializeApp();
+    console.log("firebase app initialized", admin.apps.length);
+  } else {
+    console.log("firebase app ALREADY initialized", admin.apps.length);
+  }
 }
 
-export class FirebaseRTDBConfigurationStore {
-  db: admin.database.Database
+export class FirebaseRTDBConfigurationStore extends BaseConfigurationStore {
+  db: admin.database.Database;
 
-  constructor(public userID: string, private globalPath = 'internal/global/', private userPath = 'internal/user/') {}
+  // constructor(public userID: string, private globalPath = 'internal/global/', private userPath = 'internal/user/') {}
 
-  private setData<T>(settingsPath: string, value: T): Promise<T> {
-    const dbPath = this.db.ref(settingsPath)
-    return dbPath.set(value).then(() => value)
+  protected setData<T>(settingsPath: string, value: T): Promise<T> {
+    const dbPath = this.db.ref(settingsPath);
+    return dbPath.set(value).then(() => value);
   }
 
-  private getData<T>(settingsPath: string, defaultValue?: T): Promise<T> {
-    const dbPath = this.db.ref(settingsPath)
+  protected getData<T>(settingsPath: string, defaultValue?: T): Promise<T> {
+    const dbPath = this.db.ref(settingsPath);
     return dbPath.once("value").then(data => {
-      const val = data.val()
-      if (val === null && defaultValue !== undefined) return this.setData(settingsPath, defaultValue)
-      return val
-    })
+      const val = data.val();
+      if (val === null && defaultValue !== undefined)
+        return this.setData(settingsPath, defaultValue);
+      return val;
+    });
   }
 
-  init(): Promise<IConfiguration> {
-    initializeFirebase()
-    this.db = admin.database()
-    return Promise.resolve(this)
-  }
-
-  private getGlobalSettingsPath(key: string): string {
-    return `${this.globalPath}${key}`
-  }
-
-  private getUserSettingsPath(key: string): string {
-    return `${this.userPath}${this.userID}/${key}`
-  }
-
-  setGlobalData<T>(key: string, value: T): Promise<T> {
-    return this.setData<T>(this.getGlobalSettingsPath(key), value)
-  }
-
-  getGlobalData<T>(key: string, defaultValue?: T): Promise<T> {
-    return this.getData<T>(this.getGlobalSettingsPath(key), defaultValue)
-  }
-
-  setUserData<T>(key: string, value: T): Promise<T> {
-    return this.setData<T>(this.getUserSettingsPath(key), value)
-  }
-
-  getUserData<T>(key: string, defaultValue?: T): Promise<T> {
-    return this.getData<T>(this.getUserSettingsPath(key), defaultValue)
+  init(): Promise<IConfigurationStore> {
+    initializeFirebase();
+    this.db = admin.database();
+    return Promise.resolve(this);
   }
 }
